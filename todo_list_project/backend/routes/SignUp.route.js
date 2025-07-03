b95db2bc6a01ee206010a8e933596284bca3fd3a
Karim Kammoun <128716985+KarimKammoun@users.noreply.github.com>
2025-07-03 22:23:31 +0100

    Delete todo_list_project directory

--- todo_list_project/backend/routes/SignUp.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { signUp} from "../controllers/SignUp.controller.js";
-
-const router = express.Router();
-
-router.post("/", signUp);
-
-export default router;
